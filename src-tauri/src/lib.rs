5e69397fbbc5fcfcd7300afcaff5adf41797d540
Prajan Karthik <prjnkrthk@gmail.com>
2026-03-10 02:06:02 +0530

    feat: Migrate HTTP client from Axios to Tauri's native HTTP plugin and update associated configurations and dependencies.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ use std::sync::Mutex;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .manage(Mutex::new(state::VaultState::default()))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
